ceaf5aed1fd0ee43d4fc0402cf090bd05c04180a
AlexeyNesterenk0 <79803933413@ya.ru>
2024-10-30 14:07:07 +0300

    upd

--- testQT.py
+++ testQT.py
@@ -1,50 +1,56 @@
-import sys, subprocess
-from PyQt5.QtWidgets import QWidget, QApplication, QLabel, QVBoxLayout
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QHBoxLayout, QPushButton
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QMovie
-from PyQt5.QtCore import Qt, QThread, pyqtSignal
 
-class PingThread(QThread):
-    finished = pyqtSignal()
-
-    def __init__(self, host, packet):
-        super().__init__()
-        self.host = host
-        self.packet = packet
-
-    def run(self):
-        process = subprocess.Popen(['ping', '-c', self.packet, self.host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        output, error = process.communicate()
-        if debug:
-            print(output)
-        self.finished.emit()
-
-class LoadingAnimation(QWidget):
+class GifViewer(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.setWindowTitle('Просмотр GIF')
         self.setGeometry(100, 100, 400, 300)
-        self.setWindowTitle('Анимация загрузки')
-        layout = QVBoxLayout()
 
-        self.loading_label = QLabel(self)
-        #self.loading_label.move(250, 10)
-        self.movie = QMovie('Spinning octopus.gif')  # Подставьте путь к вашему анимационному GIF
+        layout = QHBoxLayout()
 
-        self.loading_label.setMovie(self.movie)
-        layout.addWidget(self.loading_label)
+        self.gif_label = QLabel(self)
+        self.start_button = QPushButton('Запустить', self)
+        self.start_button.clicked.connect(self.start_animation)
+        self.stop_button = QPushButton('Остановить', self)
+        self.stop_button.clicked.connect(self.stop_animation)
 
+        #current_dir = os.getcwd()
+        root_folder = '.'  # Начальная папка для поиска GIF файлов (текущая папка)
+        gif_path = self.find_gif_in_subfolders(root_folder)
+
+        self.movie = QMovie(gif_path)
+        self.gif_label.setMovie(self.movie)
         self.movie.start()
 
+        layout.addWidget(self.gif_label, alignment= Qt.AlignRight)
+        layout.addWidget(self.start_button)
+        layout.addWidget(self.stop_button)
+
         self.setLayout(layout)
-        self.setWindowFlag(Qt.WindowStaysOnTopHint)  # Устанавливаем виджет поверх других окон
+
+    def stop_animation(self):
+        if self.movie.state() != QMovie.NotRunning:
+            self.movie.stop()
+
+    def start_animation(self):
+        if self.movie.state() == QMovie.NotRunning:
+            self.movie.start()
+
+
+    def find_gif_in_subfolders(self, folder):
+        for dirpath, _, filenames in os.walk(folder):
+            for filename in filenames:
+                if filename.endswith('Spinning octopus.gif'):
+                    return os.path.join(dirpath, filename)
+        return None
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = LoadingAnimation()
-    window.show()
-
-    ping_thread = PingThread('192.168.10.110', '100')
-    ping_thread.finished.connect(window.movie.stop)
-    
-    ping_thread.start()
-
+    viewer = GifViewer()
+    viewer.show()
     sys.exit(app.exec_())
