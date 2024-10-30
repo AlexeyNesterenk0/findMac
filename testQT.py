6e2e59bf449682a96736db7c478340b66aec4d05
AlexeyNesterenk0 <79803933413@ya.ru>
2024-10-30 10:27:58 +0300

    testLoading

--- testQT.py
+++ testQT.py
@@ -1,49 +1,31 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMessageBox, QLabel, QLineEdit, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QApplication, QDialog, QLabel, QVBoxLayout
+from PyQt5.QtGui import QMovie
+from PyQt5.QtCore import Qt
 
-class App(QWidget):
+class LoadingAnimation(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.setWindowTitle('Анимация загрузки')
+        layout = QVBoxLayout()
 
-    def initUI(self):
-        self.setGeometry(100, 100, 400, 300)
-        self.setWindowTitle('Поиск устройства по MAC')
+        self.loading_label = QLabel(self)
+        self.movie = QMovie('loading_animation.gif')  # Подставьте путь к вашему анимационному GIF
 
-        btn = QPushButton('Поиск', self)
-        btn.setToolTip('Запуск поиска')
-        btn.resize(btn.sizeHint())
-        btn.move(310, 250)
-        btn.clicked.connect(self.on_button_click)
+        self.loading_label.setMovie(self.movie)
+        layout.addWidget(self.loading_label)
 
-        exit_btn = QPushButton('Выход', self)
-        exit_btn.setToolTip('Завершить приложение')
-        exit_btn.resize(exit_btn.sizeHint())
-        exit_btn.move(200, 250)
-        exit_btn.clicked.connect(self.exit_button_click)
+        self.movie.start()
 
-        self.label = QLabel('', self)
-        self.label.setGeometry(50, 50, 100, 100)
-
-        self.label.move(10, 10)
-
-        self.line_edit = QLineEdit(self)
-        self.line_edit.resize(self.line_edit.sizeHint())
-        self.line_edit.move(10, 100)
-
-        self.show()
-
-    def set_label_text(self, text):
-        self.label.setText(text)
-
-    def on_button_click(self):
-        input_text = self.line_edit.text()
-        self.set_label_text(input_text)
-
-    def exit_button_click(self):
-        sys.exit()
+        self.setLayout(layout)
+        self.setWindowFlag(Qt.WindowStaysOnTopHint)  # Устанавливаем виджет поверх других окон
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = App()
+    window = LoadingAnimation()
+    window.show()
+
+    # Ваш процесс или задача
+    # ...
+    
     sys.exit(app.exec_())
